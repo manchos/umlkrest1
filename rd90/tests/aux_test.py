e6edcf01b961ebd0796ee769a46bd3fba129c6d6
manchos <vladm.ru@gmail.com>
2018-07-25 00:32:58 +0300

    attempt to test of get_equivalent_amount

--- rd90/tests/aux_test.py
+++ rd90/tests/aux_test.py
@@ -5,6 +5,7 @@ import sympy
 
 from hazard_substance.models import HazardousChemical
 from django.test import TestCase
+import rd90.calc.depths as depths
 
 
 @pytest.fixture(params=[
@@ -21,7 +22,7 @@ def test_get_k4(k4_param):
     wind_speed_input, expected_k4 = k4_param
     assert expected_k4 == aux.get_k4(wind_speed=wind_speed_input)
 
-
+"[[0.0, 0.3, 0.6, 1.0, 1.4],[0.9, 1.0, 1.0, 1.0, 1.0]]"
 @pytest.fixture(params=[
     ((20, 1, '[[0,0.3,0.6,1,1.4],[0.9,1,1,1,1]]', 'gas_no_pressure'), 1.0),
     ((20, 2, '[[0,0.3,0.6,1,1.4],[0.9,1,1,1,1]]', 'gas_no_pressure'), 1.0),
@@ -76,26 +77,53 @@ def test_get_evaporation_duration(evaporation_duration_param):
     ).in_hours
 
 
-# aux.get_evaporation_duration(chemical_thickness=)
+
+
+import csv
+from collections import namedtuple
+chem_dict ={}
+HazardousChemical = namedtuple('HazardousChemical', 'id, name, form, gas_density, liquid_density, boiling_t, toxodeth, k1, k2, k3, k7, descr')
+for chem in map(HazardousChemical._make, csv.reader(open('rd90/fixtures/HazardousChemicals.csv', "r"), delimiter=';')):
+    if chem.name in ('Хлор', 'Аммиак хранение под давлением', 'Аммиак изотермическое хранение'):
+        k5 =1 # инверсия
+        chem_dict[chem.name] = chem
+
+@pytest.fixture(params=[
+    ((chem_dict['Аммиак изотермическое хранение'].k1,
+      chem_dict['Аммиак изотермическое хранение'].k3, 1, 1, 40, 1), 0.74),
+    # ((1.8, 1.553, 0.052, 1.33, 1), 40.42),
+], ids=['Аммиак изотермическое хранение'])
+def equivalent_amount_param(request):
+    return request.param
+
+def test_get_equivalent_amount(equivalent_amount_param):
+    params, expected_equivalent_amount= equivalent_amount_param
+    k1, k3, k5, k7, chemical_amount, cloud_number = params
+    assert expected_equivalent_amount == aux.get_equivalent_amount(
+        k1=k1, k3=k3, k5=k5, k7=k7, chemical_amount=chemical_amount, cloud_number=cloud_number
+    )
+
+
+
 
 # @pytest.fixture(params=[
-#     ((20, 1, '[[0,0.3,0.6,1,1.4],[0.9,1,1,1,1]]', 'gas_no_pressure'), 1.0),
-#     ((20, 2, '[[0,0.3,0.6,1,1.4],[0.9,1,1,1,1]]', 'gas_no_pressure'), 1.0),
-#     ((-25, 1, '[[0.3,0.5,0.8,1,1.2],[1,1,1,1,1]]', 'gas_no_pressure'), 0.45)
-# ], ids=['', '', ''])
-# def evaporation_duration_param(request):
+#     (4, 2.0),
+#     (5, 2.34),
+#     (2, 1.33),
+#     (8.5, 3.505),
+#     (17.3, 5.68)
+# ], ids=['', '', '', '', ''])
+# def contamination_depth_param(request):
 #     return request.param
 #
-# def get_evaporation_duration(evaporation_duration_param):
-#     params, expected_evaporation_duration = evaporation_dur
-#         density=density,
-#         k2=k2,ation_param
-#     layer_thickness, density, k2, k4, k7 = params
-#     assert expected_evaporation_duration == aux.get_evaporation_duration(
-#         layer_thickness=layer_thickness,
-#         k4=k4,
-#         k7=k7,
-#     )
+# def test_get_contamination_depth(contamination_depth_param):
+#
+# depths.get_contamination_depth(
+#     equivalent_amount=,
+#     wind_speed=,
+# )
+
+
 
 # @pytest.fixture(scope='session')
 # def django_db_modify_db_settings():
